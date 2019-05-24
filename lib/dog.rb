812e25222f4d9e5be514df0ba1d72356905174c3
Alethea Cary <aletheacary@gmail.com>
2019-05-24 03:23:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,7 +15,7 @@ class Dog
   end
   
   def self.all
-    @all.collect{|instance| name = instance.split(/["@name=">]/)}
+    @all.each{|instance| name = instance.split(/["@name=">]/)}
      puts name[1]
    
   end
